10144c1b7f7a7efcc629f1973ee2a163d698db8b
GZPlaz <123790074+GZPlaz@users.noreply.github.com>
2025-07-02 18:42:41 -0500

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,5 @@
+<?php
+$_BETHEL['dbServer'] = 'ep-mute-base-a4ncmotg.us-east-1.pg.koyeb.app';
+$_BETHEL['dbName'] = 'koyebdb';
+$_BETHEL['dbUser'] = 'koyeb-adm';
+$_BETHEL['dbPassword'] = 'npg_jmTC5oYf4LXK';
